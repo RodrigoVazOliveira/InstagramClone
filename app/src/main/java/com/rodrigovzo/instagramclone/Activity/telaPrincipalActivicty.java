d2fa0f77d69a40e4f3f13a594dc406ce8985692e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2018-06-18 23:05:19 -0300

    criado sistema de abas e inserido icones das pages

--- app/src/main/java/com/rodrigovzo/instagramclone/Activity/telaPrincipalActivicty.java
+++ app/src/main/java/com/rodrigovzo/instagramclone/Activity/telaPrincipalActivicty.java
@@ -1,6 +1,7 @@
 package com.rodrigovzo.instagramclone.Activity;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.parse.ParseUser;
 import com.rodrigovzo.instagramclone.Adapter.TabsAdapter;
@@ -28,13 +30,22 @@ public class telaPrincipalActivicty extends AppCompatActivity {
         slidingTabLayout = findViewById(R.id.telaprincipal_sliding_tab_layout);
         viewPager        = findViewById(R.id.telaprincipal_view_pager);
 
-        TabsAdapter tabsAdapter =  new TabsAdapter(getSupportFragmentManager(), this);
-
         // configurando a toolbar
         toolbarPrincipal = findViewById(R.id.toolbar_principal);
         toolbarPrincipal.setLogo(R.drawable.instagramlogo);
-
         setSupportActionBar( toolbarPrincipal );
+
+
+        // configurando abas
+        TabsAdapter tabsAdapter =  new TabsAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(tabsAdapter);
+        slidingTabLayout.setCustomTabView(R.layout.tabs_view, R.id.text_item_view);
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.indicador));
+        slidingTabLayout.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        slidingTabLayout.setViewPager(viewPager);
+
+
     }
 
     @Override
--- app/src/main/java/com/rodrigovzo/instagramclone/Adapter/TabsAdapter.java
+++ app/src/main/java/com/rodrigovzo/instagramclone/Adapter/TabsAdapter.java
@@ -1,26 +1,69 @@
 package com.rodrigovzo.instagramclone.Adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.SpannedString;
+import android.text.style.ImageSpan;
+
+import com.rodrigovzo.instagramclone.Fragments.HomeFragment;
+import com.rodrigovzo.instagramclone.Fragments.UsuarioFragment;
+import com.rodrigovzo.instagramclone.R;
 
 public class TabsAdapter extends FragmentPagerAdapter {
 
     private Context context;
+    //private String[] abas = new String[]{"HOME", "USUARIO"};
+    private Integer[] icones = new Integer[]{R.drawable.ic_action_home, R.drawable.ic_perm_contact_calendar };
+    private int tamanhoicone;
 
     public TabsAdapter(FragmentManager fm, Context c) {
         super(fm);
         this.context  = c;
+        double escala = this.context.getResources().getDisplayMetrics().density;
+        this.tamanhoicone = (int) (36 * escala);
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        Fragment fragment = null;
+
+        switch (position){
+            case 0:
+                fragment = new HomeFragment();
+                break;
+            case 1:
+                fragment = new UsuarioFragment();
+                break;
+        }
+
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return icones.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        // definindo icone
+        Drawable drawable = ContextCompat.getDrawable(this.context, icones[position]);
+        drawable.setBounds(0,0, this.tamanhoicone, this.tamanhoicone);
+
+        // convertendo imagem para ser exibida
+        ImageSpan imageSpan = new ImageSpan(drawable);
+        SpannableString spannableString = new SpannableString(" ");
+        spannableString.setSpan(imageSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return spannableString;
     }
 }
