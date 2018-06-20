3cf375fcb5cb607ec03132febf9bd6c4f3c51f15
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2018-06-20 01:41:46 -0300

    Feito implementação de postagem de fotos com galeria e salvar no Parse

--- app/src/main/java/com/rodrigovzo/instagramclone/Activity/telaPrincipalActivicty.java
+++ app/src/main/java/com/rodrigovzo/instagramclone/Activity/telaPrincipalActivicty.java
@@ -1,6 +1,9 @@
 package com.rodrigovzo.instagramclone.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +13,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.provider.MediaStore.Images;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.rodrigovzo.instagramclone.Adapter.TabsAdapter;
 import com.rodrigovzo.instagramclone.R;
 import com.rodrigovzo.instagramclone.Util.SlidingTabLayout;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class telaPrincipalActivicty extends AppCompatActivity {
 
@@ -42,7 +58,7 @@ public class telaPrincipalActivicty extends AppCompatActivity {
         slidingTabLayout.setCustomTabView(R.layout.tabs_view, R.id.text_item_view);
         slidingTabLayout.setDistributeEvenly(true);
         slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.indicador));
-        slidingTabLayout.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+      //  slidingTabLayout.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
         slidingTabLayout.setViewPager(viewPager);
 
 
@@ -65,9 +81,70 @@ public class telaPrincipalActivicty extends AppCompatActivity {
             case R.id.action_configuracaoes:
                 return true;
             case R.id.action_compartilhar:
+                compartilhaFoto();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void compartilhaFoto(){
+
+        // abrir galeria de fotos
+        Intent intent = new Intent(Intent.ACTION_PICK, Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            // SALVAR FOTO
+
+            Uri localImagemSelecionada = data.getData();
+            try {
+
+                // momntando a imagem
+                Bitmap images = Images.Media.getBitmap( getContentResolver() ,localImagemSelecionada);
+                // COMPRIMIR IMAGEM PNG
+                ByteArrayOutputStream stream  = new ByteArrayOutputStream();
+                images.compress(Bitmap.CompressFormat.PNG,75,stream);
+
+
+                // montando o arquivo de iamgem para ser salvo no banco de dados
+                byte[] byteArray = stream.toByteArray();
+                // criar um arquivo com formato própio
+                SimpleDateFormat dataformat = new SimpleDateFormat("ddmmaaaahhmmss");
+                String nomeImage            = dataformat.format(new Date());
+                ParseFile arquivoParse = new ParseFile("image-" + nomeImage +".png", byteArray);
+
+                // salvar no banco/servidor
+                ParseObject parseObject = new ParseObject("imagem");
+                parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                parseObject.put("imagem", arquivoParse);
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null){
+                            Toast.makeText(getApplicationContext(),
+                                        "Sua imagem foi postada com sucesso!",
+                                    Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(getApplicationContext(),
+                                    "Ocorreu um erro na postagem! " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
         }
 
     }
